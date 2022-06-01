de4971a4e025a881338783933d0b61bb752fc7ef
Jone23 <jone23@rambler.ru>
2022-06-01 07:07:01 +0300

    Start project

---
+++ app/src/main/java/com/jone232323/tetrisgame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jone232323.tetrisgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
